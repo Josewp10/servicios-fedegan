07a3255da68effb05849ebb16f02422ff00e8767
JoseOnniDev <josemarulanda.onni@gmail.com>
2021-10-02 18:25:44 -0500

    Configuración inicial

---
+++ src/config/default.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+module.exports={
+    SERVER:{
+        port:3001
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,30 @@
+process.env.NODE_CONFIG_DIR = './src/config';
+
+//Import dependencies
+const express = require('express');
+const cors = require('cors');
+const config = require('config');
+
+//Initialize express
+const app = express();
+
+
+app.use(express.json());
+app.use(cors());
+
+//Endpoint
+app.get('/', (req,res) =>{
+    res.send('API servicios fedegan');
+});
+
+//routes
+
+//Port
+const port = config.get('SERVER.port');
+
+//Levantamiento
+ app.listen(port || 5000, () => {
+    console.log(`Escuchando API en http://localhost:${port}`);
+ });
+
+ 
